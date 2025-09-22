892a2b1757d40daa100f0aefeeafbd0dc2f00fc5
greatonoja <greatsglob@gmail.com>
2025-09-22 20:25:03 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections
+}
